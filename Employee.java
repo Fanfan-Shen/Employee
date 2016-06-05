5758f372cb4f3c712c4436a19fbab85ac9680193
Fanfan-Shen <1217485070@qq.com>
2016-06-05 14:49:49 +0800

    using List do a litter demo

---
+++ Employee.java
@@ -0,0 +1,37 @@
+package com.liren.sf.test;
+
+public class Employee {
+	
+	private int employeeno;
+	
+	private String employeename;
+	
+	public Employee(int employeeno2,String employeename){
+		
+		this.employeeno=employeeno2;
+		
+		this.employeename=employeename;
+		
+		
+		
+	}
+
+	public int getEmployeeno() {
+		return employeeno;
+	}
+
+	public void setEmployeeno(int employeeno) {
+		this.employeeno = employeeno;
+	}
+
+	public String getEmployeename() {
+		return employeename;
+	}
+
+	public void setEmployeename(String employeename) {
+		this.employeename = employeename;
+	}
+	
+	    
+
+}
